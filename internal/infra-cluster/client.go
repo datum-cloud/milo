34b46147fde2a206495e80b37bad312c701c25e4
Scot Wells <wells.scot@gmail.com>
2025-06-12 19:35:17 -0500

    feat: support using in-cluster config
    This changes the controller manager to use the
    in-cluster config by default to connect to the
    infrastructure cluster. This can be adjusted by
    specifying the file path to a kubeconfig using the
    `--infra-cluster-kubeconfig` flag.

--- internal/infra-cluster/client.go
+++ internal/infra-cluster/client.go
@@ -1,6 +1,8 @@
 package infracluster
 
 import (
+	"fmt"
+
 	"github.com/spf13/pflag"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -13,13 +15,17 @@ type Options struct {
 }
 
 func (o *Options) AddFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&o.KubeconfigFile, "infra-cluster-kubeconfig", "", "The path to the kubeconfig file for the infrastructure cluster.")
+	fs.StringVar(&o.KubeconfigFile, "infra-cluster-kubeconfig", "-", "The path to the kubeconfig file for the infrastructure cluster. Use '-' to use the in-cluster config.")
 }
 
 func (o *Options) GetClient() (*rest.Config, error) {
+	if o.KubeconfigFile == "-" {
+		return rest.InClusterConfig()
+	}
+
 	config, err := clientcmd.BuildConfigFromFlags("", o.KubeconfigFile)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to build config from flags: %w", err)
 	}
 	return config, nil
 }
