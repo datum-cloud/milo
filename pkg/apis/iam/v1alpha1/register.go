f5f221ed037c30fac743e14a5e6d712cf0fbc938
Jose Szychowski <77838520+JoseSzycho@users.noreply.github.com>
2025-07-11 11:32:54 -0300

    feat: enhance scheme registration by adding internal known types for MachineAccountKey

--- pkg/apis/iam/v1alpha1/register.go
+++ pkg/apis/iam/v1alpha1/register.go
@@ -10,11 +10,28 @@ import (
 var SchemeGroupVersion = schema.GroupVersion{Group: "iam.miloapis.com", Version: "v1alpha1"}
 
 var (
-	// SchemeBuilder initializes a scheme builder
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// SchemeBuilder initializes a scheme builder. We register both the external
+	// v1alpha1 types and internal (unversioned) types so that the generic
+	// apiserver storage layer, which converts objects to the internal version
+	// before persisting them, can encode/decode MachineAccountKey resources.
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes, addInternalKnownTypes)
 	// AddToScheme is a global function that registers this API group & version to a scheme
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// addInternalKnownTypes registers the IAM types with the scheme using the
+// "internal" API version. We currently only need MachineAccountKey and its
+// list to satisfy the storage codec used by the apiserver, but additional
+// types can be added here as they get first-class storage implementations.
+func addInternalKnownTypes(scheme *runtime.Scheme) error {
+	internalGV := schema.GroupVersion{Group: SchemeGroupVersion.Group, Version: runtime.APIVersionInternal}
+
+	scheme.AddKnownTypes(internalGV,
+		&MachineAccountKey{},
+		&MachineAccountKeyList{},
+	)
+	return nil
+}
 
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
