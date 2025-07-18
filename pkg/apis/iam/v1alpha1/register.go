d7f20659485ea29fcbbbb4731f5bc61f22ec9494
Jose Szychowski <77838520+JoseSzycho@users.noreply.github.com>
2025-07-18 11:14:44 -0300

    feat: Register UserDeactivation and UserDeactivationList types

--- pkg/apis/iam/v1alpha1/register.go
+++ pkg/apis/iam/v1alpha1/register.go
@@ -39,6 +39,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&MachineAccountKeyList{},
 		&UserPreference{},
 		&UserPreferenceList{},
+		&UserDeactivation{},
+		&UserDeactivationList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
