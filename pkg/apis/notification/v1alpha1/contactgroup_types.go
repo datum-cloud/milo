6808f8e901520711c24c6999e4ef15679a29ad90
Jose Szychowski <joseszychowski@gmail.com>
2025-09-22 11:33:16 -0300

    feat: add new ContactGroup resource

---
+++ pkg/apis/notification/v1alpha1/contactgroup_types.go
@@ -0,0 +1,87 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Create conditions
+const (
+	// ContactGroupReadyCondition is the condition Type that tracks contact group creation status.
+	ContactGroupReadyCondition = "Ready"
+	// ContactGroupCreatePendingReason is used when contact group creation is in progress.
+	ContactGroupCreatePendingReason = "CreatePending"
+	// ContactGroupCreatedReason is used when contact group creation succeeds.
+	ContactGroupCreatedReason = "CreateSuccessful"
+)
+
+// Delete conditions
+const (
+	// ContactGroupDeletedCondition is the condition Type that tracks contact group deletion status.
+	ContactGroupDeletedCondition = "Delete"
+	// ContactGroupDeletePendingReason is used when contact group deletion is in progress.
+	ContactGroupDeletePendingReason = "DeletePending"
+	// ContactGroupDeletedReason is used when contact group deletion succeeds.
+	ContactGroupDeletedReason = "DeleteSuccessful"
+)
+
+// Update conditions
+const (
+	// ContactGroupUpdatedCondition is the condition Type that tracks contact group update status.
+	ContactGroupUpdatedCondition = "Update"
+	// ContactGroupUpdatePendingReason is used when contact group update is in progress.
+	ContactGroupUpdatePendingReason = "UpdatePending"
+	// ContactGroupUpdatedReason is used when contact group update succeeds.
+	ContactGroupUpdatedReason = "UpdateSuccessful"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// ContactGroup is the Schema for the contactgroups API.
+// It represents a logical grouping of Contacts.
+// +kubebuilder:printcolumn:name="DisplayName",type="string",JSONPath=".spec.displayName"
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:scope=Namespaced
+type ContactGroup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ContactGroupSpec   `json:"spec,omitempty"`
+	Status ContactGroupStatus `json:"status,omitempty"`
+}
+
+// ContactGroupSpec defines the desired state of ContactGroup.
+// +kubebuilder:validation:Type=object
+type ContactGroupSpec struct {
+	// DisplayName is the display name of the contact group.
+	// +kubebuilder:validation:Required
+	DisplayName string `json:"displayName,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ContactGroupList contains a list of ContactGroup.
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ContactGroupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ContactGroup `json:"items"`
+}
+
+type ContactGroupStatus struct {
+	// Conditions represent the latest available observations of an object's current state.
+	// Standard condition is "Ready" which tracks contact group creation status and sync to the contact group provider.
+	// +kubebuilder:default={{type: "Ready", status: "Unknown", reason: "CreatePending", message: "Waiting for contact group to be created", lastTransitionTime: "1970-01-01T00:00:00Z"}}
+	// +kubebuilder:validation:Optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// ProviderID is the identifier returned by the underlying contact groupprovider
+	// (e.g. Resend) when the contact groupis created. It is usually
+	// used to track the contact creation status (e.g. provider webhooks).
+	// +optional
+	ProviderID string `json:"providerID,omitempty"`
+}
--- pkg/apis/notification/v1alpha1/register.go
+++ pkg/apis/notification/v1alpha1/register.go
@@ -25,6 +25,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&EmailList{},
 		&Contact{},
 		&ContactList{},
+		&ContactGroup{},
+		&ContactGroupList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
