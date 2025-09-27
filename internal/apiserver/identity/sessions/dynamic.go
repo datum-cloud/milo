886de2984d8aa5ccec39a013bb17324da9aa4048
Zach Smith <zachdsmith2015@gmail.com>
2025-09-26 21:26:51 -0700

    feat: implement public virtual session api

---
+++ internal/apiserver/identity/sessions/dynamic.go
@@ -0,0 +1,132 @@
+package sessions
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	identityv1alpha1 "go.miloapis.com/milo/pkg/apis/identity/v1alpha1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	authuser "k8s.io/apiserver/pkg/authentication/user"
+	apirequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+)
+
+type Config struct {
+	BaseConfig             *rest.Config
+	ProviderGVR            schema.GroupVersionResource
+	Timeout                time.Duration
+	Retries                int
+	ImpersonateExtrasAllow map[string]struct{}
+}
+
+type DynamicProvider struct {
+	base        *rest.Config
+	gvr         schema.GroupVersionResource
+	to          time.Duration
+	retries     int
+	allowExtras map[string]struct{}
+}
+
+func NewDynamicProvider(cfg Config) (*DynamicProvider, error) {
+	if cfg.BaseConfig == nil {
+		return nil, fmt.Errorf("base rest.Config is required")
+	}
+	return &DynamicProvider{
+		base:        rest.CopyConfig(cfg.BaseConfig),
+		gvr:         cfg.ProviderGVR,
+		to:          cfg.Timeout,
+		retries:     cfg.Retries,
+		allowExtras: cfg.ImpersonateExtrasAllow,
+	}, nil
+}
+
+func (b *DynamicProvider) dynForUser(ctx context.Context) (dynamic.Interface, error) {
+	u, ok := apirequest.UserFrom(ctx)
+	if !ok || u == nil {
+		return nil, fmt.Errorf("no user in context")
+	}
+	cfg := rest.CopyConfig(b.base)
+	if b.to > 0 {
+		cfg.Timeout = b.to
+	}
+	extras := map[string][]string{}
+	for k, v := range u.GetExtra() {
+		if _, ok := b.allowExtras[k]; ok {
+			extras[k] = v
+		}
+	}
+	cfg.Impersonate = rest.ImpersonationConfig{
+		UserName: u.GetName(),
+		UID:      u.GetUID(),
+		Groups:   u.GetGroups(),
+		Extra:    extras,
+	}
+	return dynamic.NewForConfig(cfg)
+}
+
+func (b *DynamicProvider) ListSessions(ctx context.Context, _ authuser.Info, opts *metav1.ListOptions) (*identityv1alpha1.SessionList, error) {
+	dyn, err := b.dynForUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var ul *unstructured.UnstructuredList
+	var lastErr error
+	for i := 0; i <= b.retries; i++ {
+		ul, lastErr = dyn.Resource(b.gvr).List(ctx, *opts)
+		if lastErr == nil {
+			break
+		}
+	}
+	if lastErr != nil {
+		return nil, lastErr
+	}
+	out := new(identityv1alpha1.SessionList)
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(ul.UnstructuredContent(), out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (b *DynamicProvider) GetSession(ctx context.Context, _ authuser.Info, name string) (*identityv1alpha1.Session, error) {
+	dyn, err := b.dynForUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var uobj *unstructured.Unstructured
+	var lastErr error
+	for i := 0; i <= b.retries; i++ {
+		uobj, lastErr = dyn.Resource(b.gvr).Get(ctx, name, metav1.GetOptions{})
+		if lastErr == nil {
+			break
+		}
+	}
+	if lastErr != nil {
+		return nil, lastErr
+	}
+	out := new(identityv1alpha1.Session)
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(uobj.UnstructuredContent(), out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (b *DynamicProvider) DeleteSession(ctx context.Context, _ authuser.Info, name string) error {
+	dyn, err := b.dynForUser(ctx)
+	if err != nil {
+		return err
+	}
+	var lastErr error
+	for i := 0; i <= b.retries; i++ {
+		lastErr = dyn.Resource(b.gvr).Delete(ctx, name, metav1.DeleteOptions{})
+		if lastErr == nil {
+			break
+		}
+	}
+	return lastErr
+}
