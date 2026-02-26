a588049a9e39c5bd6533660770c07399879a0463
OscarLlamas6 <oscarllamasusac@gmail.com>
2026-02-25 23:19:47 -0600

    debug: test field label conversion in init

--- internal/apiserver/storage/identity/storageprovider.go
+++ internal/apiserver/storage/identity/storageprovider.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	serverstorage "k8s.io/apiserver/pkg/server/storage"
+	"k8s.io/klog/v2"
 	controlplaneapiserver "k8s.io/kubernetes/pkg/controlplane/apiserver"
 
 	sessionsregistry "go.miloapis.com/milo/internal/apiserver/identity/sessions"
@@ -38,6 +39,28 @@ func init() {
 		&metav1.APIGroup{},
 		&metav1.APIResourceList{},
 	)
+
+	// Wrap the scheme's ConvertFieldLabel to add logging
+	originalConverter := Scheme.Converter()
+	if originalConverter != nil {
+		// Note: We can't actually wrap ConvertFieldLabel easily, but we can verify registration
+		// by checking if the scheme recognizes our GVKs
+		userIdentityGVK := identityv1alpha1.SchemeGroupVersion.WithKind("UserIdentity")
+		sessionGVK := identityv1alpha1.SchemeGroupVersion.WithKind("Session")
+
+		// Test if field label conversion works
+		if _, _, err := Scheme.ConvertFieldLabel(userIdentityGVK, "status.userUID", "test"); err == nil {
+			klog.InfoS("UserIdentity field label conversion is working in init", "gvk", userIdentityGVK.String())
+		} else {
+			klog.ErrorS(err, "UserIdentity field label conversion FAILED in init", "gvk", userIdentityGVK.String())
+		}
+
+		if _, _, err := Scheme.ConvertFieldLabel(sessionGVK, "status.userUID", "test"); err == nil {
+			klog.InfoS("Session field label conversion is working in init", "gvk", sessionGVK.String())
+		} else {
+			klog.ErrorS(err, "Session field label conversion FAILED in init", "gvk", sessionGVK.String())
+		}
+	}
 }
 
 type StorageProvider struct {
